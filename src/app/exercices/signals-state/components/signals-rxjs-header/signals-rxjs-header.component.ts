e4e72fe7fdbdad57aa416e4bfbd4e90ba5ae3b96
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-10-01 08:58:07 +0200

    feat: update exercices

--- src/app/exercices/signals-state/components/signals-rxjs-header/signals-rxjs-header.component.ts
+++ src/app/exercices/signals-state/components/signals-rxjs-header/signals-rxjs-header.component.ts
@@ -2,13 +2,15 @@ import {Component, inject} from '@angular/core';
 import {SignalsRxjsService} from "../../signals-rxjs.service";
 import {AsyncPipe} from "@angular/common";
 import {RouterLink} from "@angular/router";
+import {MatBadge} from "@angular/material/badge";
 
 @Component({
   selector: 'app-signals-rxjs-header',
   standalone: true,
   imports: [
     AsyncPipe,
-    RouterLink
+    RouterLink,
+    MatBadge
   ],
   templateUrl: './signals-rxjs-header.component.html',
   styleUrl: './signals-rxjs-header.component.css'
--- src/app/exercices/signals-state/signals-rxjs.service.ts
+++ src/app/exercices/signals-state/signals-rxjs.service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject, take} from "rxjs";
+import {Product} from "./models/product.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignalsRxjsService {
-  readonly #basketSubject = new BehaviorSubject([]);
+  readonly #basketSubject: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
   readonly basket$ = this.#basketSubject.asObservable();
 
   constructor() { }
 
-  addItem(item: any) {
+  addItem(product: Product) {
     this.basket$.pipe(take(1)).subscribe(value => {
-      this.#basketSubject.next(value);
+      this.#basketSubject.next([
+        ...value,
+        product
+      ]);
     });
   }
 
-  removeItem(item: any) {
+  removeItem(product: Product) {
     this.basket$.pipe(take(1)).subscribe(value => {
       this.#basketSubject.next(value);
     });
