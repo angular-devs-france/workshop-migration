1d15b2f1f2f5ef1259b791b9f4387f81c486b90e
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-10-01 13:54:46 +0200

    feat: notification

--- src/app/exercices/signals-state/container.component.ts
+++ src/app/exercices/signals-state/container.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {HeaderComponent} from "./components/header/header.component";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {CartService} from "./cart.service";
 
 @Component({
   selector: 'app-container',
@@ -11,6 +13,13 @@ import {HeaderComponent} from "./components/header/header.component";
   ],
   templateUrl: './container.component.html'
 })
-export class ContainerComponent {
+export class ContainerComponent implements OnInit {
+  snackBar = inject(MatSnackBar);
+  cartService = inject(CartService);
 
+  ngOnInit(): void {
+    this.cartService.cart$.subscribe(() => {
+      this.snackBar.open('Cart updated!', '',  {duration: 1000})
+    });
+  }
 }
