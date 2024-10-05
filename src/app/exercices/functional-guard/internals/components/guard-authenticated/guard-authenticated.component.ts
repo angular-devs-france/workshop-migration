371e1c35d6922e83ab2d6ad5c1343ad0c895f30e
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-10-05 12:20:43 +0200

    refactor: update functional-guard

---
+++ src/app/exercices/functional-guard/internals/components/guard-authenticated/guard-authenticated.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guard-authenticated',
+  standalone: true,
+  imports: [],
+  templateUrl: './guard-authenticated.component.html',
+  styleUrl: './guard-authenticated.component.css'
+})
+export class GuardAuthenticatedComponent {
+
+}
