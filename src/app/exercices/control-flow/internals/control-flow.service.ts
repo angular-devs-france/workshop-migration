aba888d2a0127251e93214ae70bce91b1019c11f
Camille Pillot <cpillot@takima.fr>
2024-10-06 19:56:10 +0200

    fix: prettier control flow

--- src/app/exercices/control-flow/internals/control-flow.service.ts
+++ src/app/exercices/control-flow/internals/control-flow.service.ts
@@ -1,22 +1,26 @@
-import {Injectable} from '@angular/core';
-import {Observable, of} from "rxjs";
-import {Product} from "../../signals-state/internals/product.model";
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from '../../signals-state/internals/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ControlFlowService {
   data = [
-    {id: 1, name: 'Bananes', price: 30},
-    {id: 2, name: 'Tomates', price: 20},
-    {id: 3, name: 'Fraises', price: 20},
-    {id: 4, name: 'Orange', price: 10},
-    {id: 5, name: 'Pomme', price: 5},
+    { id: 1, name: 'Bananes', price: 30 },
+    { id: 2, name: 'Tomates', price: 20 },
+    { id: 3, name: 'Fraises', price: 20 },
+    { id: 4, name: 'Orange', price: 10 },
+    { id: 5, name: 'Pomme', price: 5 },
   ];
 
   search(searchTerm: string): Observable<Product[]> {
-    return searchTerm.length ?
-      of(this.data.filter(x => x.name.toLowerCase().includes(searchTerm.toLowerCase()))):
-      of(this.data);
+    return searchTerm.length
+      ? of(
+          this.data.filter((x) =>
+            x.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        )
+      : of(this.data);
   }
 }
